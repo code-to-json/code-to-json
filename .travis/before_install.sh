c9769ae03b949f82b999949135d9ff48ecd89972
Mike North <michael.l.north@gmail.com>
2018-10-12 00:11:10 -0700

    fix: 🐛 correct reference to repo in before_install CI hook

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -11,7 +11,7 @@ if [[ $TRAVIS_BRANCH == 'master' ]]; then
   rm -rf .git
   git init
   git clean -dfx
-  git remote add origin https://github.com/atlassian/lerna-semantic-release.git
+  git remote add origin https://github.com/mike-north/code-to-json.git
   git fetch origin
   git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
   git checkout $TRAVIS_BRANCH
