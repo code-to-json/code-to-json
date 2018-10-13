b1f4e2649a67247c6ab4dfbe15db940ea810aec6
Mike North <michael.l.north@gmail.com>
2018-10-12 23:08:56 -0700

    chore(ci): more debugging in CI scripts

---
+++ scripts/ci/publish.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "git status"
+git status
+echo "adding all files for commit"
+git add -A
+echo "git status"
+git status
+echo "attempting publish"
+./node_modules/.bin/lerna publish --message \"chore: update changelogs [skip-ci]\" --yes
---
+++ scripts/test/ci.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+./node_modules/.bin/commitlint-travis && ./node_modules/.bin/lerna run lint && ./node_modules/.bin/lerna run test
