3ea88ac6839e665df3ff0b18b6e9300a88060da7
Mike North <michael.l.north@gmail.com>
2018-10-12 21:46:53 -0700

    fix: update walker comments

--- packages/core/src/walker.ts
+++ packages/core/src/walker.ts
@@ -1,3 +1,7 @@
 import ts from 'typescript';
 
+/**
+ * Walk a typescript program, using specified entry points, returning
+ * JSON information about the code
+ */
 export function walkProgram(project: ts.Program, entries: string[]) {}
