9edac5f66e8c6b6fa5db74b6b25f75831a4e6eec
Mike North <michael.l.north@gmail.com>
2018-10-13 17:36:19 -0700

    chore(ci): publish confirmation always answers yes

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -2,4 +2,4 @@
 echo "git status"
 git status
 echo "lerna publish"
-./node_modules/.bin/lerna publish --conventional-commits --message "chore: update changelogs [skip-ci]"
+./node_modules/.bin/lerna publish --conventional-commits --message "chore: update changelogs [skip-ci]" --yes
