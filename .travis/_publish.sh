1f592c053515453cccca93b434d53f4eea530fc2
Mike North <michael.l.north@gmail.com>
2018-10-13 17:27:22 -0700

    chore(ci): temporarily disable publishing

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 echo "git status"
 git status
-echo "lerna publish"
-./node_modules/.bin/lerna version --conventional-commits --yes
-./node_modules/.bin/lerna publish --message \"chore: update changelogs [skip-ci]\"
+# echo "lerna publish"
+# ./node_modules/.bin/lerna publish --message \"chore: update changelogs [skip-ci]\"
