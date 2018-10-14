45ec3517050eecf017f536d1a81ae441e5c88c15
Mike North <michael.l.north@gmail.com>
2018-10-13 17:31:25 -0700

    chore(ci): fix lerna publish command

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 echo "git status"
 git status
-# echo "lerna publish"
-# ./node_modules/.bin/lerna publish --message \"chore: update changelogs [skip-ci]\"
+echo "lerna publish"
+./node_modules/.bin/lerna publish --conventional-commits --message "chore: update changelogs [skip-ci]"
