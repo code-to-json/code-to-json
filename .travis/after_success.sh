7ed1445c01e97aad28ff44037826058ea0fb0573
Mike North <michael.l.north@gmail.com>
2018-10-20 10:31:33 -0700

    fix: guard CI publish step with travis env variables

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-# ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "We are in a pull request, skipping publish"
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  echo "On master branch. Proceeding with publish"
+  ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
+fi
