2bbff63e802c0326286aa6ea06b2478dda3a132f
Mike North <michael.l.north@gmail.com>
2018-10-19 22:44:13 -0700

    fix: better typing on processing queue

--- packages/core/src/serializers/type.ts
+++ packages/core/src/serializers/type.ts
@@ -1,4 +1,4 @@
-import { Flags, flagsToString } from '@code-to-json/utils';
+import { Flags, flagsToString, getObjectFlags } from '@code-to-json/utils';
 import * as ts from 'typescript';
 import { ProcessingQueue } from '../processing-queue';
 import { isRef, SymbolRef, TypeRef } from '../processing-queue/ref';
@@ -11,6 +11,13 @@ export interface SerializedType {
   aliasTypeArguments?: TypeRef[];
   aliasSymbol?: SymbolRef;
   flags?: Flags;
+  objectFlags?: Flags;
+  defaultType?: TypeRef;
+  numberIndexType?: TypeRef;
+  constraint?: TypeRef;
+  stringIndexType?: TypeRef;
+  properties?: SymbolRef[];
+  baseTypes?: TypeRef[];
 }
 
 export default function serializeType(
@@ -20,8 +27,8 @@ export default function serializeType(
   queue: ProcessingQueue
 ): SerializedType {
   const { flags, aliasSymbol, aliasTypeArguments, symbol } = typ;
-
-  const typeData = {
+  const objFlags = getObjectFlags(typ);
+  const typeData: SerializedType = {
     id: ref.id,
     thing: 'type' as 'type',
     symbol: queue.queue(symbol, 'symbol', checker),
@@ -32,7 +39,43 @@ export default function serializeType(
         .map((ata) => queue.queue(ata, 'type', checker))
         .filter(isRef),
     aliasSymbol: aliasSymbol && queue.queue(aliasSymbol, 'symbol', checker),
-    flags: flagsToString(flags, 'type')
+    flags: flagsToString(flags, 'type'),
+    objectFlags: objFlags ? flagsToString(objFlags, 'object') : undefined
   };
+
+  const numberIdxType = typ.getNumberIndexType();
+  if (numberIdxType) {
+    typeData.numberIndexType = queue.queue(numberIdxType, 'type', checker);
+  }
+  const stringIdxType = typ.getNumberIndexType();
+  if (stringIdxType) {
+    typeData.stringIndexType = queue.queue(stringIdxType, 'type', checker);
+  }
+  const defaultType = typ.getDefault();
+  if (defaultType) {
+    typeData.defaultType = queue.queue(defaultType, 'type', checker);
+  }
+  const constraint = typ.getConstraint();
+  if (constraint) {
+    typeData.constraint = queue.queue(constraint, 'type', checker);
+  }
+  const baseTypes = typ.getBaseTypes();
+  if (baseTypes) {
+    typeData.baseTypes = baseTypes
+      .map((bt) => queue.queue(bt, 'type', checker))
+      .filter(isRef);
+  }
+  // if (
+  //   typ.symbol &&
+  //   typ.symbol.valueDeclaration &&
+  //   typ.symbol.valueDeclaration.kind !== ts.SyntaxKind.SourceFile
+  // ) {
+  //   if (properties && properties.length) {
+  //     typeData.properties = properties
+  //       .map((prop) => queue.queue(prop, 'symbol', checker))
+  //       .filter(isRef);
+  //   }
+  // }
+
   return typeData;
 }
