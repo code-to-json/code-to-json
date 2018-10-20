29cf6c3a35d81db85b6038b61d3cef51f4067218
Mike North <michael.l.north@gmail.com>
2018-10-20 10:38:07 -0700

    chore: reorganize travis-ci scripts

--- .travis/after_success.sh
+++ .travis/after_success.sh
@@ -1,11 +1,2 @@
 #!/bin/bash
-
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, skipping publish"
-  exit 0
-fi
-
-if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  echo "On master branch. Proceeding with publish"
-  ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
-fi
+# After Success
