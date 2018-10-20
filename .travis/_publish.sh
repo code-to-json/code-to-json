fa18fbbe911040a08f5069b05dede410ed517b58
Mike North <michael.l.north@gmail.com>
2018-10-20 10:38:07 -0700

    chore: reorganize travis-ci scripts

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-echo "git status"
-git status
-echo "lerna publish"
-./node_modules/.bin/lerna publish --conventional-commits --message "chore: update changelogs [skip ci]" --yes
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "We are in a pull request, skipping publish"
+  exit 0
+fi
+
+if [[ $TRAVIS_BRANCH == 'master' ]]; then
+  echo "On master branch. Proceeding with publish"
+  echo "git status"
+  git status
+  echo "lerna publish"
+  ./node_modules/.bin/lerna publish --conventional-commits --message "chore: update changelogs [skip ci]" --yes
+fi
