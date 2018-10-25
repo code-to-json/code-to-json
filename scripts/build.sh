ae83e286ec66a6d2a5269920935471e6b4614aba
Mike North <michael.l.north@gmail.com>
2018-10-25 15:36:49 -0700

    chore: link before build

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+./node_modules/.bin/lerna link
 tsc -b packages/schema && tsc -b packages/utils && tsc -b packages/core && tsc -b packages/formatter && tsc -b packages/cli
--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 lerna clean --yes
 rm -rf packages/*/lib
+rm -rf packages/*/.nyc_output
+rm -rf packages/*/coverage
