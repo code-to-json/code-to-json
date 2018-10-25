7632206f9228fd92e400b944a01fa8f09b782db5
Mike North <michael.l.north@gmail.com>
2018-10-25 10:17:00 -0700

    chore: code coverage

--- packages/utils/test/first.test.ts
+++ packages/utils/test/first.test.ts
@@ -1,15 +1,12 @@
 import { suite, test } from 'mocha-typescript';
 import * as snapshot from 'snap-shot-it';
+import { isEmpty } from '../src/checks';
 
-function add(x: number, y: number) { return x + y; }
 
 @suite
 class Two {
   @test
-  // tslint:disable-next-line:typedef
   public method() {
-    snapshot(add(103, 20));
-    snapshot('a text message');
-    return Promise.resolve(42).then(snapshot);
+    snapshot({ result: isEmpty(0)});
   }
 }
