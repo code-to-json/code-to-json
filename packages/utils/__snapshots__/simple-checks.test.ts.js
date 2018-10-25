26c861a44d6e3ab08d4ca6fb35b7f555a262fa52
Mike North <michael.l.north@gmail.com>
2018-10-25 11:04:58 -0700

    chore: tests can be run without coverage reporting

---
+++ packages/utils/__snapshots__/simple-checks.test.ts.js
@@ -0,0 +1,107 @@
+exports['Simple predicates isBlank tests 1'] = {
+  "result": false
+}
+
+exports['Simple predicates isBlank tests 2'] = {
+  "result": true
+}
+
+exports['Simple predicates isBlank tests 3'] = {
+  "result": true
+}
+
+exports['Simple predicates isBlank tests 4'] = {
+  "result": true
+}
+
+exports['Simple predicates isBlank tests 5'] = {
+  "result": false
+}
+
+exports['Simple predicates isBlank tests 6'] = {
+  "result": true
+}
+
+exports['Simple predicates isBlank tests 7'] = {
+  "result": false
+}
+
+exports['Simple predicates isBlank tests 8'] = {
+  "result": false
+}
+
+exports['Simple predicates isBlank tests 9'] = {
+  "result": false
+}
+
+exports['Simple predicates isEmpty tests 1'] = {
+  "result": false
+}
+
+exports['Simple predicates isEmpty tests 2'] = {
+  "result": true
+}
+
+exports['Simple predicates isEmpty tests 3'] = {
+  "result": true
+}
+
+exports['Simple predicates isEmpty tests 4'] = {
+  "result": true
+}
+
+exports['Simple predicates isEmpty tests 5'] = {
+  "result": false
+}
+
+exports['Simple predicates isEmpty tests 6'] = {
+  "result": true
+}
+
+exports['Simple predicates isEmpty tests 7'] = {
+  "result": false
+}
+
+exports['Simple predicates isEmpty tests 8'] = {
+  "result": false
+}
+
+exports['Simple predicates isEmpty tests 9'] = {
+  "result": false
+}
+
+exports['Simple predicates isPresent tests 1'] = {
+  "result": true
+}
+
+exports['Simple predicates isPresent tests 2'] = {
+  "result": false
+}
+
+exports['Simple predicates isPresent tests 3'] = {
+  "result": false
+}
+
+exports['Simple predicates isPresent tests 4'] = {
+  "result": false
+}
+
+exports['Simple predicates isPresent tests 5'] = {
+  "result": true
+}
+
+exports['Simple predicates isPresent tests 6'] = {
+  "result": false
+}
+
+exports['Simple predicates isPresent tests 7'] = {
+  "result": true
+}
+
+exports['Simple predicates isPresent tests 8'] = {
+  "result": true
+}
+
+exports['Simple predicates isPresent tests 9'] = {
+  "result": true
+}
