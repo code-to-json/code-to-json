0e52b94022eb9db5a3b766cad7ae16c3786ce035
Mike North <michael.l.north@gmail.com>
2018-10-26 09:25:20 -0700

    chore: add starting point for tests

---
+++ packages/test-helpers/test/first.test.ts
@@ -0,0 +1,15 @@
+import { suite, test } from 'mocha-typescript';
+import * as snapshot from 'snap-shot-it';
+
+function add(x: number, y: number) { return x + y; }
+
+@suite
+class Two {
+  @test
+  // tslint:disable-next-line:typedef
+  public method() {
+    snapshot(add(103, 20));
+    snapshot('a text message');
+    return Promise.resolve(42).then(snapshot);
+  }
+}
