092c989dc1bfe5717c2e850e9718adc2286482cc
Mike North <michael.l.north@gmail.com>
2018-10-27 20:27:56 -0700

    chore(ci): never setup git in pr-push builds

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,31 +3,33 @@
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "We are in a pull request, not setting up release"
   exit 0
-else if [[ $TRAVIS_BRANCH == 'master' ]]; then
-  rm -rf .git
-  git init
-  git clean -dfx
-  git remote add origin https://github.com/mike-north/code-to-json.git
-  git fetch origin
-  # git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
-  git checkout $TRAVIS_BRANCH
-  git config credential.helper store
-  echo "https://mike-north:${GH_TOKEN}@github.com/mike-north/code-to-json.git" > ~/.git-credentials
+else
+ if [[ $TRAVIS_BRANCH == 'master' ]]; then
+    rm -rf .git
+    git init
+    git clean -dfx
+    git remote add origin https://github.com/mike-north/code-to-json.git
+    git fetch origin
+    # git clone https://github.com/$TRAVIS_REPO_SLUG.git $TRAVIS_REPO_SLUG
+    git checkout $TRAVIS_BRANCH
+    git config credential.helper store
+    echo "https://mike-north:${GH_TOKEN}@github.com/mike-north/code-to-json.git" > ~/.git-credentials
 
-  npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
-  npm prune
-  echo "npm whoami"
-  npm whoami
+    npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
+    npm prune
+    echo "npm whoami"
+    npm whoami
 
-  git config --global user.email "michael.l.north@gmail.com"
-  git config --global user.name "Mike North"
-  git config --global push.default simple
+    git config --global user.email "michael.l.north@gmail.com"
+    git config --global user.name "Mike North"
+    git config --global push.default simple
 
-  git fetch --tags
-  git branch -u origin/$TRAVIS_BRANCH
-  git fsck --full #debug
-  echo "npm whoami"
-  npm whoami #debug
-  echo "git config --list"
-  git config --list #debug
+    git fetch --tags
+    git branch -u origin/$TRAVIS_BRANCH
+    git fsck --full #debug
+    echo "npm whoami"
+    npm whoami #debug
+    echo "git config --list"
+    git config --list #debug
+  fi
 fi
