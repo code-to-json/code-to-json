575c83a524b3320a7b18b7f62b388b4467ea9941
Mike North <michael.l.north@gmail.com>
2018-10-27 22:27:12 -0700

    chore(ci): log more in travis

--- .travis/script.sh
+++ .travis/script.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 npm install -g codecov
 if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, not setting up release"
+  echo "We are in a pull request (PR $TRAVIS_PULL_REQUEST), not setting up release"
   npm run test:ci
-else
+elif if [ "$TRAVIS_BRANCH" == "master" ]; then
   echo "We are on master. Attempting publish after successful tests"
-  npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
+  npm run test:ci 
+  #&& ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
+else 
+  echo "Non master, non-pr build"
 fi
