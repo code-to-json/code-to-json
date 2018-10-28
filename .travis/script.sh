8e8cb415a77b4543435d1b9843873a5bf138ab6f
Mike North <michael.l.north@gmail.com>
2018-10-27 20:02:48 -0700

    chore(ci): better logging in CI script

--- .travis/script.sh
+++ .travis/script.sh
@@ -4,5 +4,6 @@ if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
   echo "We are in a pull request, not setting up release"
   npm run test:ci
 else
+  echo "We are on master. Attempting publish after successful tests"
   npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
 fi
