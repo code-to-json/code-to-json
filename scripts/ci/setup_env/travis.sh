be64d2f7dd857040d6906b64b077f086f10cb645
Mike North <michael.l.north@gmail.com>
2018-10-27 23:01:49 -0700

    chore(ci): centralize scripts

---
+++ scripts/ci/setup_env/travis.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+echo "=== Code-To-JSON [Setup-Env: Travis-CI] ==="
+
+if [ "$TRAVIS_PULL_REQUEST" != "false" ]
+then
+  echo "Travis: PR detected"
+  export IS_PULL_REQUEST=true
+else
+  echo "Travis: Non-PR detected"
+  export IS_PULL_REQUEST=false
+fi
+
+export CI_BRANCH="$TRAVIS_BRANCH"
--- scripts/ci/setup_tools.sh
+++ scripts/ci/setup_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm install -g ts-node yarn
+npm install -g ts-node yarn codecov
 echo "Node version: $(node -v)"
 echo "Npm version: $(node -v)"
 echo "Yarn version: $(yarn -v)"
