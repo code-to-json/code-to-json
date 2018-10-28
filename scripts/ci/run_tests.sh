70b9dfdd0224d83f1710c9557da00c4ecb884c70
Mike North <michael.l.north@gmail.com>
2018-10-27 23:21:11 -0700

    chore(ci): commitlint in travis

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 echo "=== Code-To-JSON [Run Tests] ==="
-npm run test:ci
+if [ "$TRAVIS" != "" ]
+then
+  ./node_modules/.bin/commitlint-travis && npm run test:ci
+else
+  npm run test:ci
+fi
