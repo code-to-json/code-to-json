7bdd00bb498d69087dfdb0cb18dc377d5b957a18
Mike North <michael.l.north@gmail.com>
2018-10-27 23:24:21 -0700

    chore(ci): scripty global install in CI

--- scripts/ci/setup_tools.sh
+++ scripts/ci/setup_tools.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-npm install -g ts-node yarn codecov
+npm install -g ts-node yarn codecov scripty
 echo "Node version: $(node -v)"
 echo "Npm version: $(node -v)"
 echo "Yarn version: $(yarn -v)"
