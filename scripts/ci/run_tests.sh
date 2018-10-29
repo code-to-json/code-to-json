7bcb71f3da4d9e0697eb492de9a517dbe3f6be69
Mike North <michael.l.north@gmail.com>
2018-10-28 18:12:03 -0700

    chore: better travis-ci logging

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 echo "=== Code-To-JSON [Run Tests] ==="
-if [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" == "false"  -a "$TRAVIS_BRANCH" == "master" ]
+if [ "$TRAVIS" != "" -a "$IS_PULL_REQUEST" == "false"  -a "$CI_BRANCH" == "master" ]
 then
     echo "TRAVIS: We are on master. Attempting publish after successful tests"
     npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
-elif [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-    echo "TRAVIS: PR build (branch)"
-
-elif [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" != "false" -a "$TRAVIS_BRANCH" == "master" ]
+elif [ "$TRAVIS" != "" -a "$IS_PULL_REQUEST" != "false" -a "$CI_BRANCH" == "master" ]
 then
     echo "TRAVIS: PR build (master)"
+    npm run test:ci
+elif [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" != "false"]
+then
+    echo "TRAVIS: PR build (branch)"
+    npm run test:ci
+elif [ "$TRAVIS" != "" ]
+then
+    echo "TRAVIS: (other build)"
     npm run test:ci
 else
     echo "NON-TRAVIS: Non master, non-pr build"
