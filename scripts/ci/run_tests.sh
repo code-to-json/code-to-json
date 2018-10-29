04945c45d4cadd45515c93f550be7897a1562b39
Mike North <michael.l.north@gmail.com>
2018-10-28 17:50:43 -0700

    chore(ci): publish after successful travis build on master

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 echo "=== Code-To-JSON [Run Tests] ==="
-if [ "$TRAVIS" != "" ]
+if [ ( "$TRAVIS" != "" ) -a  ( "$TRAVIS_PULL_REQUEST" == "false" ) -a ( "$TRAVIS_BRANCH" == "master" ) ]
 then
-  ./node_modules/.bin/commitlint-travis && bash ./scripts/test/ci.sh
-  if [ ( "$TRAVIS_PULL_REQUEST" == "false" ) -a ( "$TRAVIS_BRANCH" == "master" ) ]
-  then
     echo "We are on master. Attempting publish after successful tests"
     npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
-  else 
+elif [ ( "$TRAVIS" != "" ) ]
     echo "Non master, non-pr build"
     npm run test:ci
-  fi
 else
   bash ./scripts/test/ci.sh
 fi
