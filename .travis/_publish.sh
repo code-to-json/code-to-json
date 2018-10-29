c59d6cd5f814aebbc6ff50bc2591e5b186b00bfb
Mike North <michael.l.north@gmail.com>
2018-10-28 18:41:10 -0700

    chore(ci): publish for non-pr master builds

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -4,4 +4,4 @@ echo "On master branch. Proceeding with publish"
 echo "git status"
 git status
 echo "lerna publish"
-# ./node_modules/.bin/lerna publish --yes
+./node_modules/.bin/lerna publish --yes
