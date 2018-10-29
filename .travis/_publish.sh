64e1ef16f0869186e1411a9b12ee37bad88fcd4d
Mike North <michael.l.north@gmail.com>
2018-10-28 17:41:42 -0700

    chore(ci): publish after successful travis build on master

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
-  echo "We are in a pull request, skipping publish"
-  exit 0
-fi
 
-if [ ( $TRAVIS_BRANCH == 'master' ) -a ( "$TRAVIS_PULL_REQUEST" == "false" ) ]; then
-  echo "On master branch. Proceeding with publish"
-  echo "git status"
-  git status
-  echo "lerna publish"
-  ./node_modules/.bin/lerna publish --yes
-fi
+echo "On master branch. Proceeding with publish"
+echo "git status"
+git status
+echo "lerna publish"
+# ./node_modules/.bin/lerna publish --yes
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-npm install -g codecov
-if [ "$TRAVIS_PULL_REQUEST" != "false" ]
-then
-  echo "We are in a pull request (PR $TRAVIS_PULL_REQUEST), not setting up release"
-  npm run test:ci
-elif if [ "$TRAVIS_BRANCH" == "master" ]
+if [ ( "$TRAVIS_PULL_REQUEST" == "false" ) -a ( "$TRAVIS_BRANCH" == "master" ) ]
 then
   echo "We are on master. Attempting publish after successful tests"
-  npm run test:ci 
-  #&& ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
+  npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
 else 
   echo "Non master, non-pr build"
+  npm run test:ci
 fi
