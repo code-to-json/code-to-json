35c7b898ac1aa10baf64ee50fdd3ab72420493ea
Mike North <michael.l.north@gmail.com>
2018-10-28 18:48:21 -0700

    chore(ci): publish for non-pr master builds

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -7,4 +7,6 @@ echo "lerna publish"
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
 echo "npm whoami"
 npm whoami
+echo "git status"
+git status
 ./node_modules/.bin/lerna publish --yes
