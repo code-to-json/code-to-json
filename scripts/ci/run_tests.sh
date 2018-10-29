a75e1847f24bb3b346eba48123d38c9eec8365fa
Mike North <michael.l.north@gmail.com>
2018-10-28 18:31:12 -0700

    chore: better travis-ci logging

--- scripts/ci/run_tests.sh
+++ scripts/ci/run_tests.sh
@@ -3,15 +3,17 @@ echo "=== Code-To-JSON [Run Tests] ==="
 echo "IS_PULL_REQUEST=$IS_PULL_REQUEST"
 echo "IS_MASTER=$IS_MASTER"
 echo "TRAVIS=$TRAVIS"
-if [ "$TRAVIS" != "" -a "$IS_PULL_REQUEST" == "false"  -a "$IS_MASTER" == "true" ]
+echo "TRAVIS_BRANCH=$TRAVIS_BRANCH"
+echo "TRAVIS_PULL_REQUEST=$TRAVIS_PULL_REQUEST"
+if [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" == "false"  -a "$TRAVIS_BRANCH" == "master" ]
 then
     echo "TRAVIS: We are on master. Attempting publish after successful tests"
     npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
-elif [ "$TRAVIS" != "" -a "$IS_PULL_REQUEST" == "true" -a "$IS_MASTER" == "true" ]
+elif [ "$TRAVIS" != "" -a "$TRAVIS_PULL_REQUEST" != "false" -a "$TRAVIS_BRANCH" == "master" ]
 then
     echo "TRAVIS: PR build (master)"
     npm run test:ci
-elif [ "$TRAVIS" != "" -a "$IS_PULL_REQUEST" == "false" -a "$IS_MASTER" == "false" ]
+elif [ "$TRAVIS" != "" -a "$TRAVIS_BRANCH" != "master" ]
 then
     echo "TRAVIS: PR build (branch)"
     npm run test:ci
