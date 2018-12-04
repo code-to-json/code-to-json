866a4edab87d35b5ef06f79a2f14aef30064509c
Mike North <michael.l.north@gmail.com>
2018-12-04 11:19:08 -0800

    chore: use travis-ci stage for deploy

--- .travis/script.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ ( "$TRAVIS_PULL_REQUEST" == "false" ) -a ( "$TRAVIS_BRANCH" == "master" ) ]
-then
-  echo "We are on master. Attempting publish after successful tests"
-  npm run test:ci && ./node_modules/.bin/travis-deploy-once .travis/_publish.sh
-else 
-  echo "Non master, non-pr build"
-  npm run test:ci
-fi
