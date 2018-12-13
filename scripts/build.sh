59910c1e67d12e7c2485cb545c4b0e1baf21ef00
Mike North <michael.l.north@gmail.com>
2018-12-13 10:49:18 -0800

    chore: let lerna decide package build order

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-./node_modules/.bin/lerna link
-tsc -b packages/test-helpers && tsc -b packages/schema && tsc -b packages/utils && tsc -b packages/core && tsc -b packages/formatter && tsc -b packages/cli
+lerna run build
