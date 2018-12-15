22f565a11890ed6cfb008105ce4a4874f835ab6d
Mike North <michael.l.north@gmail.com>
2018-12-15 13:42:19 -0800

    chore: TS Linting fixes

--- packages/schema/test/first.test.ts
+++ packages/schema/test/first.test.ts
@@ -1,13 +1,15 @@
 import { suite, test } from 'mocha-typescript';
 import * as snapshot from 'snap-shot-it';
 
-function add(x: number, y: number) { return x + y; }
+function add(x: number, y: number): number {
+  return x + y;
+}
 
 @suite
 class Two {
   @test
   // tslint:disable-next-line:typedef
-  public method() {
+  public method(): Promise<void> {
     snapshot(add(103, 20));
     snapshot('a text message');
     return Promise.resolve(42).then(snapshot);
