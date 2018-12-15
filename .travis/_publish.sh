c0dbecd8b1665d5ffd269cb7de326e062a817fbd
Mike North <michael.l.north@gmail.com>
2018-12-14 21:16:16 -0800

    fix: restore publish script

--- .travis/_publish.sh
+++ .travis/_publish.sh
@@ -1,24 +1,13 @@
 #!/bin/bash
 
 echo "On master branch. Proceeding with publish"
-
-rm -rf .git
-git init
-git clean -dfx
-git remote add origin https://github.com/mike-north/code-to-json.git
-git fetch origin
-git checkout $TRAVIS_BRANCH
-
 echo "git status"
 git status
-
 echo "lerna publish"
 npm config set //registry.npmjs.org/:_authToken=$NPM_TOKEN -q
-npm prune
-
 echo "npm whoami"
 npm whoami
-
+git checkout $TRAVIS_BRANCH
 echo "git status"
 git status
 git config credential.helper store
@@ -28,6 +17,5 @@ git config --global push.default simple
 echo "https://mike-north:${GH_TOKEN}@github.com/mike-north/code-to-json.git" > ~/.git-credentials
 echo "git config --list"
 git config --list #debug
-yarn --non-interactive
 yarn build
 ./node_modules/.bin/lerna publish --yes
