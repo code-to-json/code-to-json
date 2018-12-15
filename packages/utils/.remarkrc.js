d3fb89c643a118f7bac2240cb37606e5178215f9
Mike North <michael.l.north@gmail.com>
2018-12-15 13:59:26 -0800

    chore: only check markdown URLs when online

--- packages/utils/.remarkrc.js
+++ packages/utils/.remarkrc.js
@@ -1,7 +1,15 @@
 exports.plugins = [
   'remark-preset-lint-recommended',
   'remark-preset-lint-markdown-style-guide',
-  ['remark-lint-no-dead-urls', [2]],
+  [
+    'remark-lint-no-dead-urls',
+    [
+      'error',
+      {
+        skipOffline: true
+      }
+    ]
+  ],
   ['remark-validate-links', { repository: 'code-to-json/code-to-json' }],
   ['remark-lint-list-item-indent', false],
   ['remark-lint-no-consecutive-blank-lines', false],
