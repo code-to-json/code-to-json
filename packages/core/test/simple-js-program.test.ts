bd3de79cf4f665f7c01008e7c3223d2ece985dbd
Mike North <michael.l.north@gmail.com>
2018-12-15 11:43:26 -0800

    chore: reorganize tests

--- packages/core/test/simple-js-program.test.ts
+++ packages/core/test/simple-js-program.test.ts
@@ -3,11 +3,6 @@ import { expect } from 'chai';
 import { suite, test } from 'mocha-typescript';
 import * as path from 'path';
 import * as snapshot from 'snap-shot-it';
-import { Node, SourceFile, Symbol as Sym, Type } from 'typescript';
-import { create as createQueue } from '../src/processing-queue';
-import { NodeRef, SourceFileRef, SymbolRef, TypeRef } from '../src/processing-queue/ref';
-import sourceFileSerializer, { SerializedSourceFile } from '../src/serializers/source-file';
-import serializeSourceFile from '../src/serializers/source-file';
 
 @suite
 class TypeScriptFixturePrograms {
@@ -36,42 +31,5 @@ class TypeScriptFixturePrograms {
     expect(nonDeclarationFiles).to.be.lengthOf(1);
 
     expect(nonDeclarationFiles[0].fileName).to.contain('src/index.js');
-  }
-
-  @test
-  public async 'snapshot of simple JS program'() {
-    const { program, rootPath } = await setupTestCase(
-      path.join(__dirname, '..', '..', '..', 'samples', 'js-single-file'),
-      ['src/index.js']
-    );
-    const checker = program.getTypeChecker();
-    const sourceFiles = program.getSourceFiles();
-    const indexFile = sourceFiles.filter(sf => !sf.isDeclarationFile)[0];
-    expect(indexFile.fileName.replace(rootPath, '')).to.eql('/src/index.js');
-
-    const q = createQueue();
-    sourceFiles.forEach(sf => {
-      return q.queue(sf, 'sourceFile', checker);
-    });
-    const serializedFileNames: string[] = [];
-    const { sourceFile: sourceFileData } = q.drain({
-      handleType(ref: TypeRef, item: Type) {
-        return checker.typeToString(item);
-      },
-      handleSymbol(ref: SymbolRef, item: Sym) {
-        return item.getName();
-      },
-      handleSourceFile(ref: SourceFileRef, item: SourceFile): SerializedSourceFile {
-        return serializeSourceFile(item, checker, ref, q);
-      }
-    });
-    const indexFilePath = Object.keys(sourceFileData).filter(k => k.startsWith(rootPath))[0];
-    const indexFileData = sourceFileData[indexFilePath];
-    expect(Object.keys(indexFileData))
-      .contains('id')
-      .contains('entity')
-      .contains('fileName')
-      .contains('symbol')
-      .contains('isDeclarationFile');
   }
 }
---
+++ packages/core/test/snapshot-smoke.test.ts
@@ -0,0 +1,63 @@
+import { setupTestCase } from '@code-to-json/test-helpers';
+import { expect } from 'chai';
+import { suite, test } from 'mocha-typescript';
+import * as path from 'path';
+import * as ts from 'typescript';
+import { SourceFile, Symbol as Sym, Type } from 'typescript';
+import { create as createQueue, DrainOutput } from '../src/processing-queue';
+import { SourceFileRef, SymbolRef, TypeRef } from '../src/processing-queue/ref';
+import { SerializedSourceFile } from '../src/serializers/source-file';
+import serializeSourceFile from '../src/serializers/source-file';
+
+@suite
+class SimpleSnapshotSmokeTests {
+  protected program!: ts.Program;
+  protected checker!: ts.TypeChecker;
+  protected rootPath!: string;
+  protected sourceFiles!: ReadonlyArray<SourceFile>;
+  protected data!: DrainOutput<string, any, {}, {}, SerializedSourceFile>;
+
+  public async before() {
+    const { program, rootPath } = await setupTestCase(
+      path.join(__dirname, '..', '..', '..', 'samples', 'js-single-file'),
+      ['src/index.js']
+    );
+    this.program = program;
+    this.rootPath = rootPath;
+    const checker = (this.checker = program.getTypeChecker());
+    const q = createQueue();
+    this.sourceFiles = program.getSourceFiles();
+    this.sourceFiles.forEach(sf => {
+      return q.queue(sf, 'sourceFile', this.checker);
+    });
+    const data = q.drain({
+      handleType(_ref: TypeRef, item: Type) {
+        return checker.typeToString(item);
+      },
+      handleSymbol(_ref: SymbolRef, item: Sym) {
+        return item.getName();
+      },
+      handleSourceFile(ref: SourceFileRef, item: SourceFile): SerializedSourceFile {
+        return serializeSourceFile(item, checker, ref, q);
+      }
+    });
+    this.data = data;
+  }
+  @test
+  public async 'SourceFile serialization'() {
+    const indexFile = this.sourceFiles.filter(sf => !sf.isDeclarationFile)[0];
+    expect(indexFile.fileName.replace(this.rootPath, ''))
+      .to.contain('src')
+      .to.contain('index.js');
+
+    const { sourceFile: sourceFileData } = this.data;
+    const indexFilePath = Object.keys(sourceFileData).filter(k => k.startsWith(this.rootPath))[0];
+    const indexFileData = sourceFileData[indexFilePath];
+    expect(Object.keys(indexFileData))
+      .contains('id')
+      .contains('entity')
+      .contains('fileName')
+      .contains('symbol')
+      .contains('isDeclarationFile');
+  }
+}
