de133009cb7fd1302fd7019153f0a3dc995d818b
Mike North <michael.l.north@gmail.com>
2018-12-17 15:28:59 -0800

    ci: fixes to TS version-testing setup

--- .travis/setup-ts-version.sh
+++ .travis/setup-ts-version.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
-echo "TS dependency found"
+echo "Setting up TS version $1"
 
 ./node_modules/.bin/lerna add "typescript@$1" --scope=@code-to-json/core --scope=@code-to-json/utils-ts --scope=@code-to-json/utils-ts --scope=@code-to-json/test-helpers --scope=@code-to-json/cli 
 ./node_modules/.bin/lerna add -D "typescript@$1" --scope=@code-to-json/utils --scope=@code-to-json/schema --scope=@code-to-json/formatter
+lerna bootstrap
+
+echo "Found Versions:"
+lerna exec "cat package.json | jq \".name\" && echo '  └ TypeScript $(tsc -v)'" --scope=@code-to-json/* --concurrency 1
