65f237297263c35d4b2cf4afcc4c90ef44b8093a
Mike North <michael.l.north@gmail.com>
2018-12-16 17:05:36 -0800

    chore: cli tests

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -15,65 +15,78 @@ process.title = 'code-to-json';
 
 export const debugLog = debug('code-to-json:cli');
 
-export function runCli(): void {
-  let actionInvoked = false;
+export async function runCli({
+  args,
+  err,
+  exit,
+}: {
+  err: (str: string) => void;
+  out: (str: string) => void;
+  args: string[];
+  exit: (code: number) => void;
+}): Promise<void> {
+  return new Promise((resolve, reject) => {
+    let actionInvoked = false;
 
-  const prog = commander
-    .name('code-to-json')
-    .arguments('[entries...]')
-    .description('a thing')
-    .option('-p,--project [path]', 'path to tsconfig.json')
-    .option('-o,--out <path>', 'output path')
-    .action((entries: string[] | undefined, _command: commander.Command) => {
-      actionInvoked = true;
-      setTimeout(() => {
-        const opts = _command.opts();
-        const { project } = opts;
-        const startupElapsed = process.hrtime(start);
-        debugLog(
-          `${chalk.yellow(
-            leftpad(
-              (
-                Math.round((startupElapsed[0] * NS_PER_SEC + startupElapsed[1]) / 1e3) / 1e3
-              ).toFixed(3),
-              6,
-            ),
-          )} ms ${chalk.green('(startup time)')}
+    const prog = commander
+      .name('code-to-json')
+      .arguments('[entries...]')
+      .description('a thing')
+      .option('-p,--project [path]', 'path to tsconfig.json')
+      .option('-o,--out <path>', 'output path')
+      .action((entries: string[] | undefined, _command: commander.Command) => {
+        actionInvoked = true;
+        setTimeout(() => {
+          const opts = _command.opts();
+          const { project } = opts;
+          const startupElapsed = process.hrtime(start);
+          debugLog(
+            `${chalk.yellow(
+              leftpad(
+                (
+                  Math.round((startupElapsed[0] * NS_PER_SEC + startupElapsed[1]) / 1e3) / 1e3
+                ).toFixed(3),
+                6,
+              ),
+            )} ms ${chalk.green('(startup time)')}
 `,
-        );
-        const beginTime = process.hrtime();
-        try {
-          run({ ...opts, project }, entries).then(() => {
-            const timeElapsed = process.hrtime(beginTime);
-            debugLog(
-              `${chalk.yellow(
-                leftpad(
-                  (Math.round((timeElapsed[0] * NS_PER_SEC + timeElapsed[1]) / 1e3) / 1e3).toFixed(
-                    3,
+          );
+          const beginTime = process.hrtime();
+          try {
+            run({ ...opts, project }, entries).then(() => {
+              const timeElapsed = process.hrtime(beginTime);
+              debugLog(
+                `${chalk.yellow(
+                  leftpad(
+                    (
+                      Math.round((timeElapsed[0] * NS_PER_SEC + timeElapsed[1]) / 1e3) / 1e3
+                    ).toFixed(3),
+                    6,
                   ),
-                  6,
-                ),
-              )} ms ${chalk.green('(extraction time)')}
+                )} ms ${chalk.green('(extraction time)')}
 `,
-            );
-          });
-        } catch (err) {
-          if (err.__invalid_arguments_error) {
-            process.stderr.write(chalk.red(`\n[ERROR] - ${err.message}\n`));
-            process.stderr.write(`${prog.help()}\n`);
-            process.exit(1);
-          } else {
-            throw err;
+              );
+            });
+          } catch (err) {
+            if (err.__invalid_arguments_error) {
+              err(chalk.red(`\n[ERROR] - ${err.message}\n`));
+              err(`${prog.help()}\n`);
+              exit(1);
+            } else {
+              throw err;
+            }
           }
-        }
-      }, 0);
-    })
-    .parse(process.argv);
+          resolve();
+        }, 0);
+      })
+      .parse(args);
 
-  if (!actionInvoked) {
-    setTimeout(() => {
-      process.stderr.write(`${prog.help()}\n`);
-      process.exit(1);
-    }, 0);
-  }
+    if (!actionInvoked) {
+      setTimeout(() => {
+        err(`${prog.help()}\n`);
+        resolve();
+        exit(1);
+      }, 0);
+    }
+  });
 }
