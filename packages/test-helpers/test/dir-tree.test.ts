640e803365419b2a20058fe05005f4bc7e78bbdb
Mike North <michael.l.north@gmail.com>
2018-12-31 19:29:42 -0800

    chore: dir tree tests

---
+++ packages/test-helpers/test/dir-tree.test.ts
@@ -0,0 +1,32 @@
+import { expect } from 'chai';
+import * as fs from 'fs';
+import { suite, test } from 'mocha-typescript';
+import * as path from 'path';
+import * as tmp from 'tmp';
+import { asString } from '../src/dir-tree';
+import { createTempFixtureFolder } from '../src/file-fixtures';
+import { setupTestCase } from '../src/index';
+
+@suite
+export class DirTreeTests {
+  @test
+  public async 'empty folder'(): Promise<void> {
+    const workspace = tmp.dirSync({ unsafeCleanup: true });
+    expect(workspace.name.length).to.be.greaterThan(0, 'Temp folder path is a non-empty string');
+    const newFolder = path.join(workspace.name, 'new-folder');
+    fs.mkdirSync(newFolder);
+    expect(asString(newFolder)).to.eq('(empty)');
+    workspace.removeCallback();
+  }
+
+  @test
+  public async 'folder with a file'(): Promise<void> {
+    const workspace = tmp.dirSync({ unsafeCleanup: true });
+    expect(workspace.name.length).to.be.greaterThan(0, 'Temp folder path is a non-empty string');
+    const newFolder = path.join(workspace.name, 'new-folder');
+    fs.mkdirSync(newFolder);
+    fs.writeFileSync(path.join(newFolder, 'file.txt'), 'Hello, world!');
+    expect(asString(newFolder).trim()).to.eq('└─ file.txt');
+    workspace.removeCallback();
+  }
+}
