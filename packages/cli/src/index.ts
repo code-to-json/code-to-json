2bfbdb4387cbacdacc8160153b3ca0cf10469a07
Mike North <michael.l.north@gmail.com>
2018-12-31 21:16:32 -0800

    chore: refactor CLI entry point

--- packages/cli/src/index.ts
+++ packages/cli/src/index.ts
@@ -5,83 +5,67 @@ import chalk from 'chalk';
 import * as commander from 'commander';
 import * as debug from 'debug';
 import * as leftpad from 'left-pad';
+import { Node } from 'typescript';
 import run from './commands/run';
 
 // tslint:disable-next-line:no-var-requires
 // const pkg = require('../../package.json');
 
 const NS_PER_SEC = 1e9;
-
-process.title = 'code-to-json';
+const PROGRAM_NAME = 'code-to-json';
+process.title = PROGRAM_NAME;
 
 export const debugLog = debug('code-to-json:cli');
+const startupElapsed = process.hrtime(start);
 
-export async function runCli({ args }: { args: string[] }): Promise<void> {
-  return new Promise<void>((resolve, reject) => {
-    debugLog('about to kick of CLI parsing');
-    let actionInvoked = false;
+export function timeString(time: [number, number], message: string): string {
+  const msg = `(${message})`;
+  return `${chalk.yellow(
+    leftpad((Math.round((time[0] * NS_PER_SEC + time[1]) / 1e3) / 1e3).toFixed(3), 6),
+  )} ms ${chalk.green(msg)}
+`;
+}
 
-    const prog = commander
-      .name('code-to-json')
-      .arguments('[entries...]')
-      .description('a thing')
-      .option('-p,--project [path]', 'path to tsconfig.json')
-      .option('-o,--out <path>', 'output path')
-      .action((entries: string[] | undefined, _command: commander.Command) => {
-        actionInvoked = true;
-        setTimeout(async () => {
-          const opts = _command.opts();
-          const { project } = opts;
-          const startupElapsed = process.hrtime(start);
-          debugLog(
-            `${chalk.yellow(
-              leftpad(
-                (
-                  Math.round((startupElapsed[0] * NS_PER_SEC + startupElapsed[1]) / 1e3) / 1e3
-                ).toFixed(3),
-                6,
-              ),
-            )} ms ${chalk.green('(startup time)')}
-`,
-          );
-          const beginTime = process.hrtime();
-          try {
-            await run({ ...opts, project }, entries).then(() => {
-              const timeElapsed = process.hrtime(beginTime);
-              debugLog(
-                `${chalk.yellow(
-                  leftpad(
-                    (
-                      Math.round((timeElapsed[0] * NS_PER_SEC + timeElapsed[1]) / 1e3) / 1e3
-                    ).toFixed(3),
-                    6,
-                  ),
-                )} ms ${chalk.green('(extraction time)')}
-`,
-              );
-            });
-          } catch (er) {
-            if (er.__invalid_arguments_error) {
-              debugLog('completing due to invalid arguments');
-              console.error(chalk.red(`\n[ERROR] - ${er.message}\n`));
-              console.error(`${prog.help()}\n`);
-              reject(new Error('invalid arguments'));
-            } else {
-              throw er;
-            }
-          }
-          resolve();
-        }, 0);
-      })
-      .parse(args);
-    debugLog('command parsed');
+function buildProgram(): commander.Command {
+  return commander
+    .name(PROGRAM_NAME)
+    .arguments('[entries...]')
+    .description('a thing')
+    .option('-p,--project [path]', 'path to tsconfig.json')
+    .option('-o,--out <path>', 'output path');
+}
 
-    if (!actionInvoked) {
-      setTimeout(() => {
-        debugLog('completing due to no action being invoked');
+function runAction(
+  prog: commander.Command,
+): (entries: string[] | undefined, cmd: commander.Command) => any {
+  return async function runActionImpl(
+    entries: string[] | undefined,
+    command: commander.Command,
+  ): Promise<void> {
+    const opts = command.opts();
+    const { project } = opts;
+    debugLog(timeString(startupElapsed, 'boot time'));
+    const beginTime = process.hrtime();
+    try {
+      await run({ ...opts, project }, entries).then(() => {
+        const timeElapsed = process.hrtime(beginTime);
+        debugLog(timeString(timeElapsed, 'extraction time'));
+      });
+    } catch (er) {
+      if (er.__invalid_arguments_error) {
+        debugLog('completing due to invalid arguments');
+        console.error(chalk.red(`\n[ERROR] - ${er.message}\n`));
         console.error(`${prog.help()}\n`);
-        reject(new Error('No action'));
-      }, 0);
+        throw new Error('invalid arguments');
+      } else {
+        throw er;
+      }
     }
-  });
+  };
+}
+
+export function runCli({ args }: { args: string[] }): void {
+  const prog = buildProgram();
+
+  prog.action(runAction(prog)).parse(args);
 }
