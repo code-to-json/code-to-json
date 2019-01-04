5c950e70dff69f5f376ebd738a31cead98220f49
Mike North <michael.l.north@gmail.com>
2019-01-03 17:15:21 -0800

    feat: formatter tests

--- packages/formatter/test/public-api-surface.test.ts
+++ packages/formatter/test/public-api-surface.test.ts
@@ -1,11 +1,18 @@
 import { expect } from 'chai';
 import { suite, test } from 'mocha-typescript';
-import { formatWalkerOutput } from '../src/index';
+import * as Exports from '../src/index';
+
+const { formatWalkerOutput } = Exports;
 
 @suite
 class PublicApiSurface {
   @test
   public 'formatWalkerOutput exists'(): void {
     expect(formatWalkerOutput).to.be.a('function');
+  }
+
+  @test
+  public 'only intended exports'(): void {
+    expect(Object.keys(Exports).sort()).to.deep.eq(['formatWalkerOutput']);
   }
 }
---
+++ packages/formatter/test/resolve-reference.test.ts
@@ -0,0 +1,88 @@
+import { createRef, Ref } from '@code-to-json/utils';
+import { expect } from 'chai';
+import { suite, test } from 'mocha-typescript';
+import { formatWalkerOutput } from '../src/index';
+import resolveReference from '../src/resolve-reference';
+
+@suite
+class ResolveReferenceTests {
+  @test
+  public type(): void {
+    const typeRef: Ref<'type'> = (createRef as any)('type', '12345');
+    const resolved = resolveReference(
+      {
+        type: {
+          '12345': {
+            id: '12345',
+          },
+        },
+      } as any,
+      typeRef,
+    );
+    expect(resolved).to.deep.eq({ id: '12345' });
+  }
+
+  @test
+  public symbol(): void {
+    const symbolRef: Ref<'symbol'> = (createRef as any)('symbol', '12345');
+    const resolved = resolveReference(
+      {
+        symbol: {
+          '12345': {
+            id: '12345',
+          },
+        },
+      } as any,
+      symbolRef,
+    );
+    expect(resolved).to.deep.eq({ id: '12345' });
+  }
+
+  @test
+  public node(): void {
+    const nodeRef: Ref<'node'> = (createRef as any)('node', '12345');
+    const resolved = resolveReference(
+      {
+        node: {
+          '12345': {
+            id: '12345',
+          },
+        },
+      } as any,
+      nodeRef,
+    );
+    expect(resolved).to.deep.eq({ id: '12345' });
+  }
+
+  @test
+  public declaration(): void {
+    const declarationRef: Ref<'declaration'> = (createRef as any)('declaration', '12345');
+    const resolved = resolveReference(
+      {
+        declaration: {
+          '12345': {
+            id: '12345',
+          },
+        },
+      } as any,
+      declarationRef,
+    );
+    expect(resolved).to.deep.eq({ id: '12345' });
+  }
+
+  @test
+  public sourceFile(): void {
+    const sourceFileRef: Ref<'sourceFile'> = (createRef as any)('sourceFile', '12345');
+    const resolved = resolveReference(
+      {
+        sourceFile: {
+          '12345': {
+            id: '12345',
+          },
+        },
+      } as any,
+      sourceFileRef,
+    );
+    expect(resolved).to.deep.eq({ id: '12345' });
+  }
+}
