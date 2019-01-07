bf7e5a0a6d5c16f855ad6558a69b7998df7aa5a8
Mike North <michael.l.north@gmail.com>
2019-01-07 09:59:42 -0800

    chore(build): update watch scripts

--- scripts/build/watch.sh
+++ scripts/build/watch.sh
@@ -2,13 +2,14 @@
 echo "Removing old build"
 rm -rf packages/*/lib
 echo "Creating initial build"
-tsc -b packages/utils && tsc -b packages/core && tsc -b packages/formatter && tsc -b packages/cli && tsc -b packages/test-helpers
+yarn build
 if [ $? -eq 0 ]; then
   echo "Setting up watch build"
   ./node_modules/.bin/concurrently \
     -n "utils,cli,core,formatter,test-helpers" \
-    -c "magenta,cyan,green,yellow,orange" \
+    -c "magenta,cyan,green,yellow,blue,orange" \
     "tsc -w -p ./packages/utils" \
+    "tsc -w -p ./packages/utils-ts" \
     "tsc -w -p ./packages/cli" \
     "tsc -w -p ./packages/core" \
     "tsc -w -p ./packages/formatter" \
