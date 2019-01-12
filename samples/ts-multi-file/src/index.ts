ac96277c755aaac7241487fe2bb442c3203fd377
Mike North <michael.l.north@gmail.com>
2019-01-12 09:49:33 -0800

    chore(samples): file-only comment [skip ci]

--- samples/ts-multi-file/src/index.ts
+++ samples/ts-multi-file/src/index.ts
@@ -1,7 +1,52 @@
 /**
  * This is the index file
+ *
+ *
+ * ## Usage
+ *
+ * This is how you use it
+ *
+ *
+ * ## Installation
+ *
+ * This is how you install it
+ *
+ * ```sh
+ * npm install foo
+ * ```
+ *
+ * ### Another thing
+ *
+ * Special installation instructins
+ *
+ * * first
+ * * second
+ * * third
+ *
+ * 1. a
+ * 1. b
+ * 1. c
+ *
+ * ![corgi](https://placecorgi.herokuapp.com/300/200)
+ *
  * @author Mike
  * @file foo
+ *
+ * @example
+ *
+ * This is how you do a thing
+ *
+ * ```ts
+ * function foo() {}
+ * ```
+ *
+ * @example
+ *
+ * And another thing
+ *
+ * ```hbs
+ * {{#if foo}} <span>bar</span> {{/if}}
+ * ```
  */
 
 /**
