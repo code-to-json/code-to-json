12581ffb5e9e1e8714f34bc293aa4ac7ac0742eb
Mike North <michael.l.north@gmail.com>
2019-01-20 13:11:29 -0800

    chore: de-duplicate eslint config

--- packages/cli/.eslintrc.js
+++ packages/cli/.eslintrc.js
@@ -1,34 +1,6 @@
 module.exports = {
-  parser: 'typescript-eslint-parser',
-  plugins: ['typescript'],
-  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: 'module',
-  },
+  extends: ['../.eslintrc.js'],
   env: {
-    es6: true,
-    browser: false,
     node: true,
   },
-  rules: {
-    camelcase: 'off',
-    'no-unused-vars': 'off',
-    'import/no-unresolved': 'off',
-    'import/export': 'off',
-    'import/first': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    'no-plusplus': 'off',
-    'import/prefer-default-export': 'off',
-    'no-console': 'off',
-  },
-  overrides: [
-    {
-      files: ['test/**/*.test.ts'],
-      rules: {
-        'class-methods-use-this': 'off',
-      },
-    },
-  ],
 };
--- packages/formatter/.eslintrc.js
+++ packages/formatter/.eslintrc.js
@@ -1,36 +1,3 @@
 module.exports = {
-  parser: 'typescript-eslint-parser',
-  plugins: ['typescript'],
-  extends: ['eslint:recommended', 'airbnb-base', 'prettier'],
-  parserOptions: {
-    ecmaVersion: 9,
-    sourceType: 'module',
-  },
-  env: {
-    es6: true,
-    browser: false,
-    node: false,
-  },
-  rules: {
-    camelcase: 'off',
-    'no-unused-vars': 'off',
-    'import/no-unresolved': 'off',
-    'import/export': 'off',
-    'import/first': 'off',
-    'import/no-extraneous-dependencies': 'off',
-    'no-underscore-dangle': 'off',
-    'no-plusplus': 'off',
-    'import/prefer-default-export': 'off',
-  },
-  overrides: [
-    {
-      files: ['test/**/*.test.ts'],
-      env: {
-        node: true,
-      },
-      rules: {
-        'class-methods-use-this': 'off',
-      },
-    },
-  ],
+  extends: ['../.eslintrc.js'],
 };
