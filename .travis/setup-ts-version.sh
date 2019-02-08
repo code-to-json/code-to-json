b474959b08edd41960c6948e73a5616654ad6108
Mike North <michael.l.north@gmail.com>
2019-02-08 15:30:51 -0800

    chore: update ts version checking script to include linkers

--- .travis/setup-ts-version.sh
+++ .travis/setup-ts-version.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Setting up TS version $1"
 
-./node_modules/.bin/lerna add "typescript@$1" --scope=@code-to-json/core --scope=@code-to-json/utils-ts  --scope=@code-to-json/test-helpers --scope=@code-to-json/cli 
+./node_modules/.bin/lerna add "typescript@$1" --scope=@code-to-json/core --scope=@code-to-json/utils-ts  --scope=@code-to-json/test-helpers --scope=@code-to-json/cli --scope=@code-to-json/core-linker --scope=@code-to-json/formatter-linker
 ./node_modules/.bin/lerna add -D "typescript@$1" --scope=@code-to-json/utils --scope=@code-to-json/comments --scope=@code-to-json/utils-node --scope=@code-to-json/schema --scope=@code-to-json/formatter
 lerna clean --yes && yarn clean && lerna bootstrap && yarn build
 
